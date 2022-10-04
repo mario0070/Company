9292efd3246c46a5b71cc149f98643bf4d833832
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2022-10-04 14:03:43 +0100

    c

---
+++ script.js
@@ -0,0 +1,8 @@
+
+//typing animation
+let typed = new Typed(".demo", {
+    strings: ["Muhammadjamiu", "Web And Mobile Developer","Search Engine Optimization", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+  })
