aa8a504c089524bf57d9c5d79bcb4e31964ade10
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2022-10-05 13:31:55 +0100

    c

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ let typed = new Typed(".demo", {
     backSpeed: 100,
     loop: true
   })
+
+
+
+window.addEventListener("scroll",reveal)
+
+function reveal(){
+    var reveals=document.querySelectorAll(".reveal")
+
+    for (var i = 0 ; i < reveals.length ;i++){
+        var windowh=window.innerHeight;
+        var revealt=reveals[i].getBoundingClientRect().top;
+        var revealp=20;
+
+        if(revealt < windowh - revealp){
+            reveals[i].classList.add("active")
+        }
+        else{
+            reveals[i].classList.remove("active")
+        }
+    }
+}
