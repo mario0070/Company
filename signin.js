168b4d2f6a35cac586ebbfbc7bc30bbb8566d462
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2022-10-15 16:02:46 +0100

    c

--- signin.js
+++ signin.js
@@ -1,11 +1,46 @@
 var passIcon=document.getElementById("passicon")
 var emailIcon=document.getElementById("emailicon")
+var passIconSuccess=document.getElementById("passicons")
+var emailIconSuccess=document.getElementById("emailicons")
 var email=document.getElementById("email")
+var password=document.getElementById("password")
+var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-function valid(){
-    if(email.value.length <= 7 ){
-        emailIcon.classList.add("valid")
-    }else{
+
+
+
+function emailValidate(){
+    if(email.value.match(validRegex)){
         emailIcon.classList.remove("valid")
+        email.classList.remove("valid")
+        emailIconSuccess.classList.add("valid")
+    }else{
+        emailIcon.classList.add("valid")
+        email.classList.add("valid")
+        emailIconSuccess.classList.remove("valid")
     }
 }
+
+function passValidate(){
+    if(password.value.length <= 5 ){
+        passIcon.classList.add("valid")
+        passIconSuccess.classList.remove("valid")
+        password.classList.add("valid")
+    }else{
+        passIcon.classList.remove("valid")
+        password.classList.remove("valid")
+        passIconSuccess.classList.add("valid")
+    }
+}
+
+function btnValidate(){
+     if(email.value.match(validRegex) && password.value.length >=6){
+        document.getElementById("btn").disabled= false
+     }else{
+        document.getElementById("btn").disabled=true
+     }
+}
+
+document.getElementById("btn").addEventListener("click",() => {
+    document.getElementById("user").innerText= email.value
+})
